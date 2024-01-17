7e5c35bf285e7d75dc9940c577952ea54b1b62d0
FatSassin <hubert@talwik.com>
2024-01-17 12:11:17 +0100

    Never Better Amigo

--- src/main/java/net/fatsassin/depthcraft/datagen/ModRecipeProvider.java
+++ src/main/java/net/fatsassin/depthcraft/datagen/ModRecipeProvider.java
@@ -1,4 +1,66 @@
 package net.fatsassin.depthcraft.datagen;
 
-public class ModRecipeProvider {
+import net.fatsassin.depthcraft.Depthcraft;
+import net.fatsassin.depthcraft.block.ModBlocks;
+import net.fatsassin.depthcraft.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
+    private static final List<ItemLike> PEARL_SMELTABLES = List.of(ModItems.PEARL.get(),
+            ModBlocks.DEATH_BLOCK.get());
+
+    public ModRecipeProvider(PackOutput pOutput) {
+        super(pOutput);
+    }
+
+    @Override
+    protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
+        oreBlasting(pWriter, PEARL_SMELTABLES, RecipeCategory.MISC, ModItems.PEARL.get(), 0.25f, 100, "pearl");
+        oreSmelting(pWriter, PEARL_SMELTABLES, RecipeCategory.MISC, ModItems.PEARL.get(), 0.25f, 200, "pearl");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.ROCK_BLOCK.get())
+                .pattern("PPP")
+                .pattern("PPP")
+                .pattern("PPP")
+                .define('P', ModItems.PETROCK.get())
+                .unlockedBy(getHasName(ModItems.PETROCK.get()), has(ModItems.PETROCK.get()))
+                .save(pWriter);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.PETROCK.get(), 9)
+                .requires(ModBlocks.ROCK_BLOCK.get())
+                .unlockedBy(getHasName(ModBlocks.ROCK_BLOCK.get()), has(ModBlocks.ROCK_BLOCK.get()))
+                .save(pWriter);
+
+
+    }
+    protected static void oreSmelting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTIme, String pGroup) {
+        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.SMELTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTIme, pGroup, "_from_smelting");
+    }
+
+    protected static void oreBlasting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
+        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.BLASTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
+    }
+
+    protected static void oreCooking(Consumer<FinishedRecipe> pFinishedRecipeConsumer, RecipeSerializer<? extends AbstractCookingRecipe> pCookingSerializer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
+        Iterator var9 = pIngredients.iterator();
+
+        while(var9.hasNext()) {
+            ItemLike itemlike = (ItemLike)var9.next();
+            SimpleCookingRecipeBuilder.generic(Ingredient.of(new ItemLike[]{itemlike}), pCategory, pResult,
+                    pExperience, pCookingTime, pCookingSerializer)
+                    .group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
+                    .save(pFinishedRecipeConsumer, Depthcraft.MODID + ":" + getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
+        }
+
+    }
 }
